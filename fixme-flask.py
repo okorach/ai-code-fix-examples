c61abe38a9612b37d570f7387178037146e62421
Olivier Korach <olivier.korach@gmail.com>
2024-12-12 15:02:37 +0100

    Add SQL injection

--- fixme-flask.py
+++ fixme-flask.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, redirect
 
 app = Flask("example")
@@ -6,3 +7,11 @@ app = Flask("example")
 def redirecting():
     url = request.args["url"]
     return redirect(url)
+
+@app.route('/user')
+def get_users():
+    user = request.args["user"]
+    sql = """SELECT user FROM users WHERE user = \'%s\'"""
+
+    conn = sqlite3.connect('example')
+    conn.cursor().execute(sql % (user)) # Noncompliant
