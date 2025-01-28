c3fc68b7d804d84229721e49eb1865f6c7958132
Olivier Korach <olivier.korach@gmail.com>
2025-01-28 10:57:30 +0100

    Update python example (#2)

--- python/python-sql-injection.py
+++ python/python-sql-injection.py
@@ -3,10 +3,15 @@ import flask
 
 app = flask.Flask("example")
 
+@app.route("/redirecting")
+def redirecting():
+    url = flask.request.args["url"]
+    return flask.redirect(url)
+
 @app.route('/user')
 def get_users():
     user = flask.request.args["user"]
-    sql = "SELECT user FROM users WHERE user = ?"
+    sql = """SELECT user FROM users WHERE user = \'%s\'"""
 
     conn = sqlite3.connect('example')
-    conn.cursor().execute(sql, (user,))
+    conn.cursor().execute(sql % (user)) # Noncompliant
